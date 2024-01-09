deaef86190f585c0769c66f5ae3af650c07a2c8c
gaurav136 <gauravtale136@gmail.com>
2024-01-10 05:27:33 +0530

    websocket

--- Qr_to_Webserver/main.py
+++ Qr_to_Webserver/main.py
@@ -1,28 +1,12 @@
-import cv2
+#!/usr/bin/env python
 
-camera_id = 0
-delay = 1
-window_name = 'OpenCV QR Code'
+import asyncio
+from websockets.sync.client import connect
 
-qcd = cv2.QRCodeDetector()
-cap = cv2.VideoCapture(camera_id)
+def hello():
+    with connect("ws://localhost:8765") as websocket:
+        websocket.send("Hello world!")
+        message = websocket.recv()
+        print(f"Received: {message}")
 
-while True:
-    ret, frame = cap.read()
-
-    if ret:
-        ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
-        if ret_qr:
-            for s, p in zip(decoded_info, points):
-                if s:
-                    print(s)
-                    color = (0, 255, 0)
-                else:
-                    color = (0, 0, 255)
-                frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
-        cv2.imshow(window_name, frame)
-
-    if cv2.waitKey(delay) & 0xFF == ord('q'):
-        break
-
-cv2.destroyWindow(window_name)
+hello()
